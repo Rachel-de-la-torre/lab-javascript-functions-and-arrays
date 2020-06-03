e9e04c03f77d139fc5fe2e36564abafb200b0a9c
Rachel de la Torre <Rachel.delatorre1@gmail.com>
2020-06-03 09:18:30 +0200

    rachel didnt finish everything but i'm still trying

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,88 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers (value1, value2){
+  if (value1 > value2){
+ return(value1);
+  } 
+    return(value2); 
+}
+
+
+
 // Iteration #2: Find longest word
+
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord (words){
+  var longestWord = "";
+  for (var i=0; i<words.length; i++) {
+    if(word[i].length > longestWord){
+      longestWord = words[i];
+    }
+  }
+ return longestWord;
+}
+
+
+
 
 // Iteration #3: Calculate the sum
 
+function sumNumbers(i){ 
+  return i + i;
+  for(let i=0; i < i.length; i++);                    
+function printResult(){
+    const result = sumNumbers[6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+    console.log(`The result is ${result}.`);
+}
+printResult();
+}
+
+//____________________________
+function sum(array) {
+  for (let sum=0, i=0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function sumNumbers(i){ 
+  return (i + i);
+  for(let i=0; i<num.sum; i++);  
+  // i / length???????????
+function printResult(){
+    const result = sumNumbers[6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+    console.log(`The result is ${result}.`);
+}
+printResult();
+}
+//_________________________
+
+function avg(array) {
+  if (array.length === 0 ) { return; }
+ 
+  for (let sum=0, i=0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum/array.length;
+}
+
+
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+
+
+
+
+
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -29,8 +99,22 @@ const wordsUnique = [
   'bring'
 ];
 
+
+
+
+
+
 // Iteration #6: Find elements
+
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+
+const doesWordExist = wordsFind.includes('trouble');
+Console.log(doesWordExist)
+
+
+
+
+
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -46,6 +130,13 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+
+
+
+
+
+
+
 
 // Iteration #8: Bonus
 
